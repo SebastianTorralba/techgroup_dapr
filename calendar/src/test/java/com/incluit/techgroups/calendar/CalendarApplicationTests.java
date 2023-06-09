07a4dd22dbeb2ea1ac61a40a1eb0b71d8615bb6d
sebastian.torralba <SebastianTorralba@coder.com.ar>
2023-06-09 02:31:32 -0300

    Inicializacion

---
+++ calendar/src/test/java/com/incluit/techgroups/calendar/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incluit.techgroups.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
