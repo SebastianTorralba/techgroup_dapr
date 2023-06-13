ab43378db5c0b6623962a62625c9c5d72c79c97e
Mateo Cerquetella <mateocerquetella@hotmail.com>
2023-06-12 23:37:13 -0300

    fix(main.py): change database URI to point to the correct database feat(main.py): add database connection check to hello_world route to ensure database connectivity

--- Study_Scheduler/main.py
+++ Study_Scheduler/main.py
@@ -1,16 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import OperationalError
+from sqlalchemy import text
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/postgres'
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://postgres:postgres@localhost:5432/academy_scheduler"
 db = SQLAlchemy(app)
 
-# Define a route for the default URL, which loads the hello world message
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-# Main driver function
-if __name__ == '__main__':
+@app.route("/")
+def hello_world():
+    try:
+        # Get a connection and execute a simple query to check the connection
+        with db.engine.connect() as connection:
+            result = connection.execute(text("SELECT 1"))
+        print("Database is connected successfully!")
+    except Exception as e:
+        print(f"Error occurred while connecting to the database: {str(e)}")
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
     # Run the application
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
