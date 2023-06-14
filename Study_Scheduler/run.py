8c46ed041070322e03aadfb33b9bba22bd6effee
Mateo Cerquetella <mateocerquetella@hotmail.com>
2023-06-14 14:34:22 -0300

    refactor(run.py): change host to 0.0.0.0 to allow access from outside the local machine refactor(docker-compose.yml): change command to run the correct file (run.py instead of main.py)

--- Study_Scheduler/run.py
+++ Study_Scheduler/run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
