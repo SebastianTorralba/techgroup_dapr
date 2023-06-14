ceddc2396f2da9ed33bed0396b0be7657c2dc843
Mateo Cerquetella <mateocerquetella@hotmail.com>
2023-06-14 15:11:21 -0300

    feat(models.py): add Academy, Classroom, Course, Module, StudyPlan, StudyPlanDetail, Subject, Teacher, User, and UserCourse models to the app feat(views.py): add GET, POST, PUT, and DELETE endpoints for StudyPlan model to the app

--- Study_Scheduler/app/study_plan/models.py
+++ Study_Scheduler/app/study_plan/models.py
@@ -1,27 +1,138 @@
-from datetime import datetime
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func, text
+from sqlalchemy.orm import relationship
 from app import db
+
+class Academy(db.Model):
+    __tablename__ = 'Academy'
+
+    id = Column(String, primary_key=True)
+    userId = Column(String, nullable=False)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+
+    classrooms = relationship('Classroom', backref='academy')
+    courses = relationship('Course', backref='academy')
+    modules = relationship('Module', backref='academy')
+
+
+class Classroom(db.Model):
+    __tablename__ = 'Classroom'
+
+    id = Column(String, primary_key=True)
+    userId = Column(String, nullable=False)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+    number = Column(String, nullable=False)
+    capacity = Column(Integer, nullable=False)
+    academyId = Column(String, ForeignKey('Academy.id'), nullable=False)
+
+
+class Course(db.Model):
+    __tablename__ = 'Course'
+
+    id = Column(String, primary_key=True)
+    userId = Column(String, nullable=False)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+    section = Column(String, nullable=False)
+    academyId = Column(String, ForeignKey('Academy.id'), nullable=False)
+
+    modules = relationship('Module', backref='course')
+    studyPlans = relationship('StudyPlan', backref='course')
+    userCourses = relationship('UserCourse', backref='course')
+
+
+class Module(db.Model):
+    __tablename__ = 'Module'
+
+    id = Column(String, primary_key=True)
+    userId = Column(String, nullable=False)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+    courseId = Column(String, ForeignKey('Course.id'), nullable=False)
+    subjectId = Column(String, ForeignKey('Subject.id'), nullable=False)
+    academyId = Column(String, ForeignKey('Academy.id'), nullable=False)
+
+    studyPlanDetails = relationship('StudyPlanDetail', backref='module')
+
 
 class StudyPlan(db.Model):
     __tablename__ = 'StudyPlan'
 
-    id = db.Column(db.String, primary_key=True, default='cuid()')
-    userId = db.Column(db.String, nullable=False)
-    createdAt = db.Column(db.DateTime, default=datetime.utcnow)
-    updatedAt = db.Column(db.DateTime, onupdate=datetime.utcnow)
-    status = db.Column(db.String, nullable=False)
-    courseId = db.Column(db.String, db.ForeignKey('course.id'))
+    id = Column(String, primary_key=True)
+    userId = Column(String, nullable=False)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+    courseId = Column(String, ForeignKey('Course.id'), nullable=False)
 
-    # Relationship
-    details = db.relationship('StudyPlanDetail', backref='plan')
+    studyPlanDetails = relationship('StudyPlanDetail', backref='studyPlan')
+
 
 class StudyPlanDetail(db.Model):
     __tablename__ = 'StudyPlanDetail'
 
-    id = db.Column(db.String, primary_key=True, default='cuid()')
-    userId = db.Column(db.String, nullable=False)
-    createdAt = db.Column(db.DateTime, default=datetime.utcnow)
-    updatedAt = db.Column(db.DateTime, onupdate=datetime.utcnow)
-    status = db.Column(db.String, nullable=False)
-    orderNo = db.Column(db.String, nullable=False)
-    moduleId = db.Column(db.String, db.ForeignKey('module.id'))
-    planId = db.Column(db.String, db.ForeignKey('StudyPlan.id'))
+    id = Column(String, primary_key=True)
+    userId = Column(String, nullable=False)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+    orderNo = Column(String, nullable=False)
+    moduleId = Column(String, ForeignKey('Module.id'), nullable=False)
+    planId = Column(String, ForeignKey('StudyPlan.id'), nullable=False)
+
+
+class Subject(db.Model):
+    __tablename__ = 'Subject'
+
+    id = Column(String, primary_key=True)
+    userId = Column(String, nullable=False)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    teacherId = Column(String, ForeignKey('Teacher.id'), nullable=False)
+
+    modules = relationship('Module', backref='subject')
+
+
+class Teacher(db.Model):
+    __tablename__ = 'Teacher'
+
+    id = Column(String, primary_key=True)
+    userId = Column(String, nullable=False)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    lastName = Column(String, nullable=False)
+
+    subjects = relationship('Subject', backref='teacher')
+
+
+class User(db.Model):
+    __tablename__ = 'User'
+
+    id = Column(String, primary_key=True)
+    userId = Column(String, nullable=False, unique=True)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+
+    userCourses = relationship('UserCourse', backref='user')
+
+
+class UserCourse(db.Model):
+    __tablename__ = 'UserCourse'
+
+    userId = Column(String, ForeignKey('User.id'), primary_key=True)
+    courseId = Column(String, ForeignKey('Course.id'), primary_key=True)
+    createdAt = Column(DateTime, nullable=False, server_default=func.now())
+    updatedAt = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
--- Study_Scheduler/app/study_plan/views.py
+++ Study_Scheduler/app/study_plan/views.py
@@ -6,8 +6,9 @@ study_plan = Blueprint('study_plan', __name__)
 
 @study_plan.route('/study-plan', methods=['GET'])
 def get_study_plans():
+    print('Study plan:', StudyPlan)
     study_plans = StudyPlan.query.all()
-    return jsonify({'study_plans': [plan.to_dict() for plan in study_plans]})
+    return jsonify({'study_plan': [plan.to_dict() for plan in study_plans]})
 
 
 @study_plan.route('/study-plan', methods=['POST'])
@@ -35,3 +36,36 @@ def create_study_plan():
     db.session.commit()
 
     return jsonify({"message": "StudyPlan created"}), 201
+
+@study_plan.route('/study-plan/<id>', methods=['GET'])
+def get_study_plan(id):
+    study_plan = StudyPlan.query.get(id)
+    if study_plan:
+        return jsonify({'study_plan': study_plan.to_dict()})
+    else:
+        return jsonify({"message": "StudyPlan not found"}), 404
+
+
+@study_plan.route('/study-plan/<id>', methods=['PUT'])
+def update_study_plan(id):
+    data = request.get_json()
+    study_plan = StudyPlan.query.get(id)
+    if study_plan:
+        study_plan.userId = data.get('userId', study_plan.userId)
+        study_plan.status = data.get('status', study_plan.status)
+        study_plan.courseId = data.get('courseId', study_plan.courseId)
+        db.session.commit()
+        return jsonify({"message": "StudyPlan updated"}), 200
+    else:
+        return jsonify({"message": "StudyPlan not found"}), 404
+
+
+@study_plan.route('/study-plan/<id>', methods=['DELETE'])
+def delete_study_plan(id):
+    study_plan = StudyPlan.query.get(id)
+    if study_plan:
+        db.session.delete(study_plan)
+        db.session.commit()
+        return jsonify({"message": "StudyPlan deleted"}), 200
+    else:
+        return jsonify({"message": "StudyPlan not found"}), 404
