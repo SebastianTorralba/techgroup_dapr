00e787df8010f9476a0ba6511b5dfde6c354e991
Mateo Cerquetella <mateocerquetella@hotmail.com>
2023-06-19 12:53:24 -0300

    Spllited postgres and sql migrations

--- db/sqlserver/entrypoint.sh
+++ db/sqlserver/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-set -e
 
-# Start SQL Server
-/opt/mssql/bin/sqlservr --accept-eula &
+# Start the SQL Server
+/opt/mssql/bin/sqlservr &
 
 # Wait for SQL Server to start
 echo "Waiting for SQL Server to start..."
 sleep 30s
 
-# Run the setup script to create the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Darp1234!" -d master -i init.sql
+# Run the SQL script
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Darp1234!' -d master -i init.sql
 
-# Wait for the SQL command to finish
-wait $!
+# Wait for SQL Server to start
+sleep 10s
+
+# Run the Node.js migration
+npm run migrate
