bfb6c6ce6bb0697404687236fc4b72e1489daede
sebastian.torralba <SebastianTorralba@coder.com.ar>
2023-06-22 10:51:44 -0300

    updagre to 1.9

--- calendar/src/test/java/com/incluit/techgroups/calendar/CalendarApplicationTests.java
+++ calendar/src/test/java/com/incluit/techgroups/calendar/CalendarApplicationTests.java
@@ -1,7 +1,14 @@
 package com.incluit.techgroups.calendar;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.domain.State;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class CalendarApplicationTests {
@@ -10,4 +17,13 @@ class CalendarApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void Test_Dapr_StateStore() {
+		DaprClient client = new DaprClientBuilder().build();
+		client.waitForSidecar(10000);
+		client.saveState("modules", "1", "Prueba").block();
+		State<String> value = client.getState("modules", "1", String.class).block();
+		assertEquals("Prueba", value.getValue());
+
+	}
 }
