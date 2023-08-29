439b8a8a6a02d3695dc2bed35bfffb228d0a6565
Mateo Cerquetella <mateocerquetella@hotmail.com>
2023-08-29 12:00:40 -0300

    fix(entrypoint.sh): fix typo in SQL Server password from 'Darp1234!' to 'Dapr1234!' for successful connection refactor(entrypoint.sh): remove unnecessary sleep command after running SQL script refactor(migration.sql): remove baseline migration file as it is no longer needed refactor(migration_lock.toml): remove migration lock file as it is no longer needed fix(package.json): fix name of new migration script from 'academy_scheduler' to 'academy_manager' for consistency

--- db/sqlserver/entrypoint.sh
+++ db/sqlserver/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Wait for SQL Server to be ready
-until /opt/mssql-tools/bin/sqlcmd -S sqlserver -U sa -P Darp1234! -Q 'SELECT 1;' >/dev/null 2>&1; do
+until /opt/mssql-tools/bin/sqlcmd -S sqlserver -U sa -P Dapr1234! -Q 'SELECT 1;' >/dev/null 2>&1; do
     echo "Waiting for SQL Server to be ready..."
     sleep 1
 done
@@ -9,7 +9,7 @@ done
 echo "SQL Server connected successfully."
 
 # Run the SQL script
-/opt/mssql-tools/bin/sqlcmd -S sqlserver -U sa -P 'Darp1234!' -d academy_manager -i init.sql
+/opt/mssql-tools/bin/sqlcmd -S sqlserver -U sa -P 'Dapr1234!' -d academy_manager -i init.sql
 
 # Wait for SQL Server to start
 sleep 10s
